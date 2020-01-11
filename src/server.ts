dcfbba4dde1cce460cc9d6391f634ab642e366b2
maksim <skarobogyodko@gmail.com>
2020-01-11 23:37:06 +0300

    fix sendDiagnostics

--- src/server.ts
+++ src/server.ts
@@ -125,9 +125,7 @@ async function validateTextDocument(textDocument: TextDocument): Promise<void> {
             []
         );
 
-        if (diagnostics.length) {
-            conn.sendDiagnostics({ uri: textDocument.uri, diagnostics });
-        }
+        conn.sendDiagnostics({ uri: textDocument.uri, diagnostics });
     } catch(err) {
         console.log(err);
     }
